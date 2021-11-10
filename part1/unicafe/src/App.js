33a1041bfef0709b4cbd814ca5e7336460af4255
Michael Bui <michaelbui99@hotmail.com>
2021-11-10 21:43:25 +0100

    Added new Header component, Button component and changed App.js to use the components

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,8 +1,29 @@
-import logo from "./logo.svg";
+import React, { useState } from "react";
+import Header from "./components/header/Header";
+import Button from "./components/button/Button";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  const [goodCount, setGoodCount] = useState(0);
+  const [neutralCount, setNeutralCount] = useState(0);
+  const [badCount, setBadCount] = useState(0);
+
+  const clickHandler = (func, state) => {
+    const handler = func(state + 1);
+    return handler;
+  };
+
+  return (
+    <div className="App">
+      <Header title="give feedback" />
+      <Button handleClick={clickHandler(goodCount, setGoodCount)} text="Good" />
+      <Button
+        handleClick={clickHandler(neutralCount, setNeutralCount)}
+        text="Neutral"
+      />
+      <Button handleClick={clickHandler(badCount, setBadCount)} text="Good" />
+    </div>
+  );
 }
 
 export default App;
