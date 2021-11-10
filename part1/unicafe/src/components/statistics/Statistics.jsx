5bf36a54b50bd4165a47d43a61018a17db8b9ffb
Michael Bui <michaelbui99@hotmail.com>
2021-11-10 22:03:46 +0100

    Feature - added display for average score and total feedback count

--- part1/unicafe/src/components/statistics/Statistics.jsx
+++ part1/unicafe/src/components/statistics/Statistics.jsx
@@ -1,10 +1,22 @@
 const Statistics = ({ goodCount, neutralCount, badCount }) => {
+  const getTotalCount = () => {
+    return goodCount + neutralCount + badCount;
+  };
+
+  const getAverageScore = () => {
+    const weightedCount = goodCount * 1 + neutralCount * 0 + badCount * -1;
+    console.log("TotalCount", getTotalCount());
+    return weightedCount / getTotalCount();
+  };
+
   return (
     <div>
       <h1>Statistics</h1>
       <p>Good {goodCount}</p>
-      <p>Neutral{neutralCount}</p>
+      <p>Neutral {neutralCount}</p>
       <p>Bad {badCount}</p>
+      <p>Total {getTotalCount()}</p>
+      <p>Average {isNaN(getAverageScore()) ? 0 : getAverageScore()}</p>
     </div>
   );
 };
