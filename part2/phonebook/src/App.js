b1e2088a8a5ce350d3d75523148584caae34b8e8
Michael Bui <michaelbui99@hotmail.com>
2021-11-15 22:00:06 +0100

    Implemented adding of new entries to the phonebook

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,7 +1,40 @@
+import React, { useState, useEffect } from "react";
+import PhoneBookEntry from "./components/PhoneBookEntry";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [newName, setNewName] = useState("");
+  const handleNameInputChange = (e) => {
+    setNewName(e.target.value);
+  };
+
+  const addName = (e) => {
+    let newPerson = {
+      name: newName,
+    };
+    setPersons(persons.concat(newPerson));
+    console.log(persons);
+    e.preventDefault();
+  };
+
+  return (
+    <div className="App">
+      <h2>Phonebook</h2>
+      <form onSubmit={addName}>
+        <div>
+          name: <input type="text" onChange={handleNameInputChange} />
+        </div>
+        <div>
+          <button type="submit">Add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {persons.map((p) => (
+        <PhoneBookEntry person={p} />
+      ))}
+    </div>
+  );
 }
 
 export default App;
