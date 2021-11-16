5b96dfb6a32af1103e4d14eef834460560ea3724
Michael Bui <michaelbui99@hotmail.com>
2021-11-16 22:33:00 +0100

    Added filtering by name to phonebook

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -3,14 +3,34 @@ import PhoneBookEntry from "./components/PhoneBookEntry";
 import "./App.css";
 
 function App() {
-  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [persons, setPersons] = useState([
+    { name: "Arto Hellas", number: "321-321321-2" },
+  ]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
+  const [nameFilter, setNameFilter] = useState("");
+  const [personsToShow, setPersonsToShow] = useState([
+    { name: "Arto Hellas", number: "321-321321-2" },
+  ]);
+
+  useEffect(() => {
+    setPersonsToShow(persons);
+  }, [persons]);
+
   const handleNameInputChange = (e) => {
     setNewName(e.target.value);
   };
   const handleNumberInputChange = (e) => {
     setNewNumber(e.target.value);
+  };
+
+  const handleFilterInputChange = (e) => {
+    //TODO: Find cause for delayed update of displayed entries when filtering.
+    setNameFilter(e.target.value, executeFilter());
+  };
+
+  const executeFilter = () => {
+    setPersonsToShow(persons.filter((p) => p.name.includes(nameFilter)));
   };
 
   const addPerson = (e) => {
@@ -41,9 +61,27 @@ function App() {
     console.log(persons);
   };
 
+  const seed = () => {
+    setPersons([
+      { name: "Arto Hellas", number: "040-123456", id: 1 },
+      { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
+      { name: "Dan Abramov", number: "12-43-234345", id: 3 },
+      { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
+    ]);
+  };
+
   return (
     <div className="App">
       <h2>Phonebook</h2>
+      <div>
+        <p>
+          Filter shown with:{" "}
+          <span>
+            <input type="text" onChange={handleFilterInputChange} />
+          </span>
+        </p>
+      </div>
+      <h2>Add new entry</h2>
       <form onSubmit={addPerson}>
         <div>
           name: <input type="text" onChange={handleNameInputChange} />
@@ -55,8 +93,9 @@ function App() {
           <button type="submit">Add</button>
         </div>
       </form>
+      <button onClick={seed}>Add Test Data</button>
       <h2>Numbers</h2>
-      {persons.map((p) => (
+      {personsToShow.map((p) => (
         <PhoneBookEntry person={p} />
       ))}
     </div>
