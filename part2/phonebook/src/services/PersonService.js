e7639133162fa9bc9825633218d5b63242dac370
Michael Bui <michaelbui99@hotmail.com>
2021-11-26 22:35:52 +0100

    Refractor busines logic of adding person to PersonService and added delete method to PersonService

--- part2/phonebook/src/services/PersonService.js
+++ part2/phonebook/src/services/PersonService.js
@@ -7,14 +7,44 @@ export default function personService() {
       return response.data;
     },
 
-    createPerson: async function (person) {
-      const response = await axios.post(this.url, person);
+    createPerson: async function (
+      person = { name: undefined, number: undefined, id: undefined }
+    ) {
+      if (
+        person.name === null ||
+        person.name === undefined ||
+        person.number === null ||
+        person.number === undefined
+      ) {
+        throw "Name or number was null or undefined";
+      }
+
+      const allPersons = await this.getAll();
+      allPersons.forEach((p) => {
+        if (p.name === person.name) {
+          throw "Person already exists";
+        }
+      });
+
+      let maxId = 0;
+      allPersons.forEach((p) => {
+        if (p.id > maxId) {
+          maxId = p.id;
+        }
+      });
+
+      const newPerson = { ...person, id: maxId + 1 };
+      const response = await axios.post(this.url, newPerson);
       return response.data;
     },
 
     update: async function (id, person) {
       const response = await axios.put(`${this.url}/${id}`, person);
       return response.data;
+    },
+
+    delete: async function (id) {
+      await axios.delete(`${this.url}/${id}`);
     },
   };
 }
